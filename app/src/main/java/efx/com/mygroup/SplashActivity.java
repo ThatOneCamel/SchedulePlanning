43be2b3f6856485c7469b39557a0a844f7977d5a
Cameron Thomas <thomascameront@gmail.com>
2018-09-29 18:28:57 -0500

    Added unversioned files

--- app/src/main/java/efx/com/mygroup/SplashActivity.java
+++ app/src/main/java/efx/com/mygroup/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this, EventData.class);
                 startActivity(intent);
                 finish();
             }
