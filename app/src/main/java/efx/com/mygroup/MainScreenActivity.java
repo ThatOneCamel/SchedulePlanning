19da7c97a45023f7145e6b918c3793c43a15c239
Quyen Dong <AznKatQuyen@ymail.com>
2018-10-05 14:36:52 -0500

    Logo was added. MainScreen Activity added. BlankFragment Activity added. EventData Activity was added.
    Signed-off-by: Quyen Dong <AznKatQuyen@ymail.com>

---
+++ app/src/main/java/efx/com/mygroup/MainScreenActivity.java
@@ -0,0 +1,19 @@
+package efx.com.mygroup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
+
+//When add button is clicked event it goes to EventData activity
