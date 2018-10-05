8a8f0780bc81d7bd9546a023b3fda90c61f3b5b2
Cameron Thomas <thomascameront@gmail.com>
2018-10-05 16:34:20 -0500

    Cameron Thomas
    +Added fragment template
    
    +Added image for fragment background
    
    -----------------
    
    Quyen Dong
    
    +Prevented users from going back to LoginActivity
    
    +Constrained FAB on Main Activity

--- app/src/main/java/efx/com/mygroup/SplashActivity.java
+++ app/src/main/java/efx/com/mygroup/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, EventData.class);
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
