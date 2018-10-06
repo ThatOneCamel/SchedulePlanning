27eac73228018417f34908f42f656a7b3e18a530
Cameron Thomas <42787639+tcameront@users.noreply.github.com>
2018-10-06 07:21:57 -0500

    + Added RecycleViewAdapter Class  This class takes the fragment template, places it in memory, and then    uses that template to populate a view with instances of customized     fragments.
     When fragments are initialized they can now be customized according
      to information from a data set.
    
     Along with this, now individual fragment data can be changed once it
      has already populated a view.
    
    + Added ViewHolder Class
    
    + Added RecyclerView to MainScreenActivity
    
    + Added RecyclerView initialization to MainScreenActivity
    
    + Added new button image
    
    - Removed ScrollView and LinearLayout from MainScreenActivity

--- app/src/main/java/efx/com/mygroup/MainScreenActivity.java
+++ app/src/main/java/efx/com/mygroup/MainScreenActivity.java
@@ -2,10 +2,11 @@ package efx.com.mygroup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -13,7 +14,42 @@ public class MainScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        createTestData();
+
+    }
+
+    //This is just a test function to create random data to test our recycled views
+    private void createTestData(){
+        ArrayList<String> randomH = new ArrayList<>();
+        ArrayList<String> randomT = new ArrayList<>();
+
+        for(int i = 0; i < 7; i++){
+            randomH.add("A" + i);
+            randomT.add("Z" + i);
+        }
+
+        initRecycler(randomH, randomT);
+
+    }
+
+    //This will initialize our custom recyclerView by telling it which RecyclerView to reference [The one in Main Activity]
+    private void initRecycler(ArrayList<String> hour, ArrayList<String> time){
+        //References the RecyclerView in the MainActivity
+        RecyclerView mainRecyclerView = findViewById(R.id.mainRecycler);
+
+        //Creates a new class object from our custom RecycleViewAdapter.Java class
+        //This is calling the constructor
+        RecycleViewAdapter mainAdapter = new RecycleViewAdapter(hour, time, this);
+
+        //Connects our recycler and our adapter
+        mainRecyclerView.setAdapter(mainAdapter);
+
+        //This will order the items correctly in a linear fashion
+        mainRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //DEBUG Statement: Called to ensure that the recycler was created without any fatal errors
+        Log.i("init called:", "Recycler created successfully");
+
     }
 }
-
-//When add button is clicked event it goes to EventData activity
