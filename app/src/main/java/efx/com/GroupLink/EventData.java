95f71222630f30d1c2b858b936f588025babdecc
Cameron Thomas <thomascameront@gmail.com>
2018-10-12 13:28:05 -0500

    Added Multi-Choice Dialog   This dialog allows users to choose which groups they want to be able    to view an event they are creating   Added pseudo-code for importation of JSON file's group names
    Fixed error affecting the time picker
      If a user input 12AM/PM, it would display as 0:mm AM/PM
        Now the input is correctly displayed
    
    Performed miscellaneous code changes in EventData
      Renamed and moved some functions around for clarity

--- app/src/main/java/efx/com/GroupLink/EventData.java
+++ app/src/main/java/efx/com/GroupLink/EventData.java
@@ -18,13 +18,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.google.firebase.auth.FirebaseUser;
+
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class EventData extends AppCompatActivity {
 
     private EditText[] input;
     private TextView alertTextView;
+    private String[] fakeGroupNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,26 +44,193 @@ public class EventData extends AppCompatActivity {
                 findViewById(R.id.fragDesc),
                 findViewById(R.id.eventDate),
                 findViewById(R.id.eventStart),
-                findViewById(R.id.eventEnd)
+                findViewById(R.id.eventEnd),
+                findViewById(R.id.eventVisibility)
         };
 
+        //Creating on click listeners for the eventStart and eventEnd text fields.
+        //When they are clicked they will create and open a new TimePickerDialog
+        //eventStart OnClickListener
         input[3].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createDateDialog(input[3]).show();
+                createTimeDialog(input[3]).show();
             }
         });
 
+        //eventEnd OnClickListener
         input[4].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createDateDialog(input[4]).show();
+                createTimeDialog(input[4]).show();
+            }
+        });
+
+        fakeGroupNames = new String[]{
+                "GroupA",
+                "GroupB",
+                "GroupC"
+        };
+
+        //eventVisibility OnClickListener
+        input[5].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createCheckboxDialog(fakeGroupNames, input[5]).show();
             }
         });
 
     }
 
-    //Create the alert box
+    //Creates the TimePickerDialog
+    public Dialog createTimeDialog(final TextView myTextView){
+        //Creating a new calendar instance with the device's current date and time
+        final Calendar c = Calendar.getInstance();
+
+        //Pulling the data from said instance and using it to pre-set the dialog picker
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int min = c.get(Calendar.MINUTE);
+
+        //Defining OnTimeSetListener, this will basically run the method below when the time has
+            // been set by the user after pressing "Ok" in the dialog
+        final TimePickerDialog.OnTimeSetListener myListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                String AM_PM;
+
+                //If the hour set is 12AM or 12PM, this ensures that the time is displayed correctly
+                switch(hourOfDay){
+                    case 0:
+                        hourOfDay = 12;
+                        AM_PM = "AM";
+                        break;
+
+                    case 12:
+                        hourOfDay = 12;
+                        AM_PM = "PM";
+                        break;
+
+                    default:
+                        if (hourOfDay < 12) {
+                            AM_PM = "AM";
+                        } else {
+                            hourOfDay -= 12;
+                            AM_PM = "PM";
+                        }
+
+                }
+
+                //Sets the text of the TextView that called for the dialog
+                myTextView.setText(String.format("%2d:%02d " + AM_PM, hourOfDay, minute));
+
+            }
+        };//End OnTimeSetListener
+
+        //This is the actual dialog that will pop up when a user initiates a TimePicker event
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                this, //Defining the context
+                R.style.TimePickerTheme, //Setting the theme
+                myListener, //Setting the OnTimeSetListener
+                hour, //Grabbing the pre-set hour
+                min, //Grabbing the pre-set minute
+                false); //Determining whether the picker is 12-hour or 24-hour
+
+        //Sets the title of the dialog and the surrounding background's appearance
+        timePickerDialog.setTitle("Choose the time:");
+        //timePickerDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        return timePickerDialog;
+
+    }// End createTimeDialog(...)
+
+    public Dialog createCheckboxDialog(String userGroups[], final TextView myTextView){
+
+        //Clearing the TextView before it is appended to
+        myTextView.setText("");
+
+        final ArrayList<Integer> selectedGroups = new ArrayList<>();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Set which groups can view this event");
+
+        builder.setMultiChoiceItems(userGroups, null, new DialogInterface.OnMultiChoiceClickListener() {
+
+
+            //What happens when the Dialog is exited by pressing "Ok"
+            //Groups will be presented by their index order, [Ex: (GroupA, GroupB, GroupC) = (0, 1, 2)
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                //If a selected item is checked, add it to the ArrayList
+                if (isChecked){
+                    selectedGroups.add(which);
+
+                //If the arrayList contains a selected item, remove it
+                } else if (selectedGroups.contains(which)){
+                    selectedGroups.remove(Integer.valueOf(which));
+                }
+            }
+
+
+        });
+
+        //When Done is touched, parse the user's choices from an integer to a string [Ex: 0 -> GroupA]
+        builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                //parseUserGroups(selectedGroups);
+                for (int i = 0; i < selectedGroups.size(); i++){
+
+                    //If 'i' is at the last index position, append the text without a comma
+                    if (i == selectedGroups.size() -1){
+                        myTextView.append(fakeGroupNames[selectedGroups.get(i)]);
+                    } else {
+
+                        //Checks the Array for the names of the groups using the selected group indexes
+                        //fakeGroupNames holds the strings
+                        //selectedGroups is being used as an index of what strings to use
+                        myTextView.append(fakeGroupNames[selectedGroups.get(i)] + ", ");
+
+                    }//End If-Else
+
+                    Log.d("Checkbox has returned:", selectedGroups.get(i).toString());
+                    dialog.dismiss();
+
+                }//End For loop
+
+
+            }
+        }); //End of Positive Button
+
+        //When Cancel is touched, close dialog
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });//End of Negative Button
+
+
+
+        return builder.create();
+    }
+
+    //Returns list of groups
+    private ArrayList<String> parseUserGroups(ArrayList<Integer> index){
+
+        ArrayList<String> userGroups = new ArrayList<>();
+        String temp;
+
+        //PseudoCode
+        /*for (int i = 0; i < index.size(); i++){
+            temp = FirebaseUserHere.GetUserGroups(index.get(i));
+            userGroups.add(temp);
+        }*/
+
+        return userGroups;
+    }
+
+
+    //Creating the error alert box
     public Dialog createDialog(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("You must fill every text slot.");
@@ -71,42 +242,8 @@ public class EventData extends AppCompatActivity {
             }
         });
 
+        //Creates the builder, waits for .show() to be called
         return builder.create();
-
-    }
-
-    public Dialog createDateDialog(final TextView myTextView){
-        final Calendar c = Calendar.getInstance();
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-        int min = c.get(Calendar.MINUTE);
-        final TimePickerDialog.OnTimeSetListener myListener = new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                String mAMPM;
-                if (hourOfDay > 13) {
-                    hourOfDay -= 12;
-                    mAMPM = "PM";
-                } else {
-                    mAMPM = "AM";
-                }
-
-                myTextView.setText(String.format("%2d:%02d " + mAMPM, hourOfDay, minute));
-
-               //myTextView.setText("h:mm a");
-            }
-        };
-
-        TimePickerDialog timePickerDialog = new TimePickerDialog(
-                this,
-                android.R.style.Theme_Holo_Dialog_NoActionBar,
-                myListener,
-                hour,
-                min,
-                false);
-
-        timePickerDialog.setTitle("Choose time of event:");
-        timePickerDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-        return timePickerDialog;
 
     }
 
